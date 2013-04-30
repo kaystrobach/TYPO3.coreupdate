3fbd7d0394201626d5f8ca2d0c71f25a91cb1988
Kay Strobach <github@kay-strobach.de>
2013-04-30 08:43:30 +0200

    [TASK] Prekäre für TYPO3 6.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,8 +36,8 @@ $EM_CONF[$_EXTKEY] = array (
 	array (
 		'depends' => 
 		array (
-			'typo3' => '4.5.0-6.0.99',
-			'fluid' => '4.5.0-6.0.99',
+			'typo3' => '4.5.0-6.1.99',
+			'fluid' => '4.5.0-6.1.99',
 		),
 		'conflicts' => 
 		array (
